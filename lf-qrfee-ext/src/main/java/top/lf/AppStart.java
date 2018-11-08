3ecf463add25ed39bee373a65f87cb9b276030f4
yuanyanm <wb198821875>
2018-11-09 07:55:10 +0800

    M

---
+++ lf-qrfee-ext/src/main/java/top/lf/AppStart.java
@@ -0,0 +1,16 @@
+package top.lf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class AppStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class, args);
+    }
+}
